8d96e7e6cc67f8c40ae705389a50cd6f7b5fdb6b
mjxjung <mj.kitty.jung@gmail.com>
2025-01-16 14:55:48 +0900

    feature: user_service del and update methods

--- app/user/user_service.py
+++ app/user/user_service.py
@@ -6,32 +6,45 @@ class UserService:
         self.repo = userRepoitory
 
     def login(self, user_login: UserLogin) -> User:
+        ## TODO
         user = self.repo.get_user_by_email(user_login.email)
-        if not user:
-            raise ValueError("User Not Found.")
-        if user_login.password != user.password:
+        print(user) # 지우기
+        if user is None:
+            raise ValueError("User not Found.")
+        elif user.password != user_login.password:
             raise ValueError("Invalid ID/PW")
-        return user
+        else:
+            return user
         
-    def regiser_user(self, new_user: User) -> User:
-        exist = self.repo.get_user_by_email(new_user.email)
-        if exist:
+    def register_user(self, new_user: User) -> User:
+        ## TODO
+        input_email_check = self.repo.get_user_by_email(new_user.email)
+        if input_email_check is not None:
             raise ValueError("User already Exists.")
-        self.repo.save_user(new_user)
-        return new_user
+        elif input_email_check is None:
+            new = self.repo.save_user(new_user)
+            print(new_user) # 지우기
+            print(new) # 지우기
+            return new
 
     def delete_user(self, email: str) -> User:
-        exist = self.repo.get_user_by_email(email)       
-        if not exist:
+        ## TODO        
+        email_check = self.repo.get_user_by_email(email)
+        if email_check is None:
             raise ValueError("User not Found.")
-        self.repo.delete_user(exist)
-        deleted_user = exist
-        return deleted_user
+        else:
+            deleted_user = self.repo.delete_user(email_check)
+            print(deleted_user) # 지우기
+            return deleted_user
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
-        exist = self.repo.get_user_by_email(user_update.email)   
-        if not exist:
-            raise ValueError("User not Found")
-        updated_user = exist
-        return updated_user
+        ## TODO
+        email_check = self.repo.get_user_by_email(user_update.email)
+        if email_check is None:
+            raise ValueError("User not Found.")
+        else:
+            email_check.password = user_update.new_password
+            self.repo.delete_user(email_check)
+            updated_user = self.repo.save_user(email_check)
+            return updated_user
         
