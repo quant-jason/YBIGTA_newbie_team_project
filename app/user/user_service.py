d98c56844abfbae4608f40032433c728c33efeeb
jason <jason02951234@gmail.com>
2025-01-16 12:53:50 +0900

    feature: user_service

--- app/user/user_service.py
+++ app/user/user_service.py
@@ -6,22 +6,32 @@ class UserService:
         self.repo = userRepoitory
 
     def login(self, user_login: UserLogin) -> User:
-        ## TODO
-        user = None
+        user = self.repo.get_user_by_email(user_login.email)
+        if not user:
+            raise ValueError("User Not Found.")
+        if user_login.password != user.password:
+            raise ValueError("Invalid ID/PW")
         return user
         
     def regiser_user(self, new_user: User) -> User:
-        ## TODO
-        new_user = None
+        exist = self.repo.get_user_by_email(new_user.email)
+        if exist:
+            raise ValueError("User already Exists.")
+        self.repo.save_user(new_user)
         return new_user
 
     def delete_user(self, email: str) -> User:
-        ## TODO        
-        deleted_user = None
+        exist = self.repo.get_user_by_email(email)       
+        if not exist:
+            raise ValueError("User not Found.")
+        self.repo.delete_user(exist)
+        deleted_user = exist
         return deleted_user
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
-        ## TODO
-        updated_user = None
+        exist = self.repo.get_user_by_email(user_update.email)   
+        if not exist:
+            raise ValueError("User not Found")
+        updated_user = exist
         return updated_user
         
