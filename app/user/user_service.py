a10d95e18a3d786c4657cb8a17f25ad9c822bc56
mjxjung <mj.kitty.jung@gmail.com>
2025-01-16 15:08:31 +0900

    refactor: user-service method

--- app/user/user_service.py
+++ app/user/user_service.py
@@ -8,43 +8,35 @@ class UserService:
     def login(self, user_login: UserLogin) -> User:
         ## TODO
         user = self.repo.get_user_by_email(user_login.email)
-        print(user) # 지우기
         if user is None:
             raise ValueError("User not Found.")
-        elif user.password != user_login.password:
+        if user.password != user_login.password:
             raise ValueError("Invalid ID/PW")
-        else:
-            return user
+        return user
         
     def register_user(self, new_user: User) -> User:
         ## TODO
-        input_email_check = self.repo.get_user_by_email(new_user.email)
-        if input_email_check is not None:
+        email_check = self.repo.get_user_by_email(new_user.email)
+        if email_check is not None:
             raise ValueError("User already Exists.")
-        elif input_email_check is None:
-            new = self.repo.save_user(new_user)
-            print(new_user) # 지우기
-            print(new) # 지우기
-            return new
+        new = self.repo.save_user(new_user)
+        return new
 
     def delete_user(self, email: str) -> User:
-        ## TODO        
+        ## TODO   
         email_check = self.repo.get_user_by_email(email)
         if email_check is None:
             raise ValueError("User not Found.")
-        else:
-            deleted_user = self.repo.delete_user(email_check)
-            print(deleted_user) # 지우기
-            return deleted_user
+        deleted_user = self.repo.delete_user(email_check)
+        return deleted_user
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
         ## TODO
         email_check = self.repo.get_user_by_email(user_update.email)
         if email_check is None:
             raise ValueError("User not Found.")
-        else:
-            email_check.password = user_update.new_password
-            self.repo.delete_user(email_check)
-            updated_user = self.repo.save_user(email_check)
-            return updated_user
+        email_check.password = user_update.new_password
+        self.repo.delete_user(email_check)
+        updated_user = self.repo.save_user(email_check)
+        return updated_user
         
